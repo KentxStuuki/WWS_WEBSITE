b060cde7d89fde73d5e4ffddbbd7f011ef7cab08
KentXStuuki <mequiambao@outlook.com>
2025-09-01 12:25:10 +0800

    Initial Commit with the organized file paths and bootstrap connectivity

---
+++ public/src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Css And Scass Connection -->
+    <link rel="stylesheet" href="../../public/main/style/main.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.theme.default.min.css">
+
+    <!-- Java Script Connection -->
+    <script src="../../public/main/scripts/bootstrap.bundle.min.js"></script> 
+    <script src="../../public/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../../public/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+    <script src="../main/scripts/data.js"></script>
+    <title>WWS</title>
+    
+</head>
+<body>
+    <h1 class="text-primary">HELLO WORLD</h1>
+</body>
+</html>
