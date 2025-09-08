400c1156b7b819f2f6decfae36bedaa35320bb8d
KentXStuuki <mequiambao@outlook.com>
2025-09-08 17:31:30 +0800

    Footer Section is tablet friendly as well

--- public/src/components/footer.php
+++ public/src/components/footer.php
@@ -1,59 +1,64 @@
-<section>
-  <footer class="bg-secondary text-white  pt-4" data-aos="fade-up" data-aos-duration="1000">
-    <div class="container ps-4 pe-5 py-3 ps-5 pe-5 gap-4">
-      <div class="row g-4 text-center text-md-start align-items-start">
-            <div class="col-12 col-md-5 col-lg-4 d-flex flex-column justify-content-between" data-aos="fade-up" data-aos-duration="1000">
-                <div>
-                <p class="fw-bold fs-5">Follow Winward Wealth Systems on Social Media</p>
-                <p class="fs-6 mb-3">
-                    We're passionate about delivering the best possible rewarding results for any money matters you wish to bring forth a solution or resolution.
-                </p>
-                </div>
-            <div class="d-flex justify-content-center justify-content-md-start gap-3 mt-5 mb-0" style="padding-top: 10%;">
-                <a href="#"><img src="../main/images/contact_section/fb.png" class="img-fluid" style="max-width:40px;" alt="Facebook"></a>
-                <a href="#"><img src="../main/images/contact_section/x.png" class="img-fluid" style="max-width:40px;" alt="X"></a>
-                <a href="#"><img src="../main/images/contact_section/ig.png" class="img-fluid" style="max-width:40px;" alt="Instagram"></a>
-                <a href="#"><img src="../main/images/contact_section/in.png" class="img-fluid" style="max-width:40px;" alt="LinkedIn"></a>
-            </div>
-            </div>
-<div class="col-6 col-md-3 col-lg-3" data-aos="fade-up" data-aos-duration="1500">
-  <p class="fw-bold fs-5">Quick Links</p>
-  <div class="footer_quick_link d-flex flex-wrap fs-6 mb-0" style="gap: 35px;">
-    <ul class="list-unstyled mb-0 py-4">
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">Home</a></li>
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">About Us</a></li>
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">Services</a></li>
-      <li><a href="#" class="text-white text-decoration-none">Testimonials</a></li>
-    </ul>
-    <ul class="list-unstyled mb-0 py-4">
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">Affiliate Program</a></li>
-      <li class="mb-2"><a href="#" class="text-white text-decoration-none">Terms & Condition</a></li>
-      <li><a href="#" class="text-white text-decoration-none">Privacy Policy</a></li>
-    </ul>
-  </div>
-</div>
+<footer class="bg-secondary text-white pt-4" data-aos="fade-up" data-aos-duration="1000">
+  <div class="container py-3 px-3 px-md-5">
+    <div class="row g-4 text-center text-md-start align-items-start">
 
-
-        <div class="col-12 col-md-4 col-lg-5" data-aos="fade-up" data-aos-duration="2000">
-          <p class="fw-bold fs-5">Feedback</p>
-          <form>
-            <div class="mb-0 py-4">
-              <h5>Full Name</h5>
-              <input type="text" class="form-control rounded-2" placeholder="Full name">
-            </div>
-            <div class="mb-3">
-                <h5>Feedback</h5>
-              <textarea class="form-control rounded-2" rows="2" placeholder="Feedback"></textarea>
-            </div>
-            <button type="submit" class="btn btn-info text-white w-100 rounded-2">Send</button>
-          </form>
+      <!-- Social / Description -->
+      <div class="col-12 col-md-12 col-lg-4 d-flex flex-column justify-content-between">
+        <div>
+          <p class="fw-bold fs-5">Follow Winward Wealth Systems on Social Media</p>
+          <p class="fs-6 mb-3">
+            We're passionate about delivering the best possible rewarding results for any money matters you wish to bring forth a solution or resolution.
+          </p>
+        </div>
+        <!-- Social Icons -->
+        <div class="d-flex justify-content-center justify-content-lg-start gap-3 mt-3 mt-lg-5 mb-0">
+          <a href="#"><img src="../main/images/contact_section/fb.png" class="img-fluid" style="max-width:40px;" alt="Facebook"></a>
+          <a href="#"><img src="../main/images/contact_section/x.png" class="img-fluid" style="max-width:40px;" alt="X"></a>
+          <a href="#"><img src="../main/images/contact_section/ig.png" class="img-fluid" style="max-width:40px;" alt="Instagram"></a>
+          <a href="#"><img src="../main/images/contact_section/in.png" class="img-fluid" style="max-width:40px;" alt="LinkedIn"></a>
         </div>
       </div>
-    </div>
 
-    <div class="text-center mt-3 pb-3 bg-white text-secondary py-3">
-      <small class="fs-6">Winward Wealth Systems &lt;W&gt; Copyright 2025 &lt;W&gt; All Rights Reserved</small>
+      <!-- Quick Links -->
+      <div class="col-6 col-md-6 col-lg-3">
+        <p class="fw-bold fs-5">Quick Links</p>
+        <div class="d-flex flex-column flex-md-row flex-wrap fs-6 mb-0 gap-3 gap-md-4 justify-content-center justify-content-md-start">
+          <ul class="list-unstyled mb-0 py-2 text-center text-md-start">
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">Home</a></li>
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">About Us</a></li>
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">Services</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Testimonials</a></li>
+          </ul>
+          <ul class="list-unstyled mb-0 py-2 text-center text-md-start">
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">Affiliate Program</a></li>
+            <li class="mb-2"><a href="#" class="text-white text-decoration-none">Terms & Condition</a></li>
+            <li><a href="#" class="text-white text-decoration-none">Privacy Policy</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Feedback Form -->
+      <div class="col-12 col-md-12 col-lg-5 mt-3 mt-lg-0">
+        <p class="fw-bold fs-5">Feedback</p>
+        <form>
+          <div class="mb-3 py-2">
+            <h5>Full Name</h5>
+            <input type="text" class="form-control rounded-2" placeholder="Full name">
+          </div>
+          <div class="mb-3">
+            <h5>Feedback</h5>
+            <textarea class="form-control rounded-2" rows="2" placeholder="Feedback"></textarea>
+          </div>
+          <button type="submit" class="btn btn-info text-white w-100 rounded-2">Send</button>
+        </form>
+      </div>
+
     </div>
-  </footer>
-</section>
+  </div>
+
+  <!-- Copyright -->
+  <div class="text-center mt-3 pb-3 bg-white text-secondary py-3">
+    <small class="fs-6">Winward Wealth Systems &lt;W&gt; Copyright 2025 &lt;W&gt; All Rights Reserved</small>
+  </div>
+</footer>
